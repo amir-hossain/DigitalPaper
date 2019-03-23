c593e5f9ee16c5f945807980ad6fec3674bbbb3a
Amir Hossain <mr.amir.1971@gmail.com>
2019-03-23 21:46:15 +0600

    gitignore modified

---
+++ app/src/main/java/amir/digital/paper/Mnanger/StaticDataManager.java
@@ -0,0 +1,33 @@
+package amir.digital.paper.Mnanger;
+
+public class StaticDataManager {
+    public static final int google_request_code =3600;
+    public static final String preference_name ="newsViews";
+    public static final String news_api_key ="7ea8ea16223e4ac18ffcad95df89a230";
+
+//    data passing key
+    public static final String isFirst_key ="isFirst";
+    public static final String column_count_key ="columnCount";
+    public static final String article_key ="article";
+    public static final String url_key ="url";
+    public static final String title_key ="title";
+
+
+//    news source name
+    public static final String google_news ="google-news";
+    public static final String ign ="ign";
+    public static final String buzzfeed ="buzzfeed";
+    public static final String mtv_news ="mtv-news";
+    public static final String medical_news ="medical-news-today";
+    public static final String bbc_news ="bbc-news";
+    public static final String bbc_sport ="bbc-sport";
+    public static final String techh_crunch ="techcrunch";
+    public static final String polygon ="polygon";
+    public static final String espn ="espn";
+    public static final String nat_geo ="national-geographic";
+    public static final String tnw ="the-next-web";
+
+//    base url
+    public static final String news_api_base_url ="https://newsapi.org/v2/";
+    public static String logged_in_key="loggedIn";
+}
